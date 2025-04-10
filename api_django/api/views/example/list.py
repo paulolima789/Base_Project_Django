d9144d81b2c40d28cc802ab3bb94c3175ae45517
paulolima789 <maicon271271@gmail.com>
2025-04-10 15:45:25 -0400

    Adiciona classe de paginação personalizada para a lista de registros de Example

--- api_django/api/views/example/list.py
+++ api_django/api/views/example/list.py
@@ -1,6 +1,8 @@
 from rest_framework.generics import ListAPIView
 from api.models import Example
 from api.serializers import ExampleListSerializer
+# paginations
+from rest_framework.pagination import PageNumberPagination
 
 # autenticated
 from rest_framework.permissions import IsAuthenticated
@@ -9,11 +11,18 @@ from api.permissions.grupos import IsExampleAdministrador, IsExamplePosVendas, I
 # from drf_yasg.utils import swagger_auto_schema
 from drf_yasg.utils import swagger_auto_schema
 
+
+class CustomPagination(PageNumberPagination):
+    page_size = 20  # Número de itens por página
+    page_size_query_param = 'page_size'  # permite o uso de ?page_size=20 na URL
+    max_page_size = 100  # limite máximo
+
 class ExampleListView(ListAPIView):
     """Lista todos os registros de Example."""
     queryset = Example.objects.all()
     serializer_class = ExampleListSerializer
     permission_classes = [IsAuthenticated, IsExampleVendas, IsExamplePosVendas, IsExampleAdministrador]
+    pagination_class = CustomPagination  # 👈 Aqui tá a mágica
 
     @swagger_auto_schema(
         operation_description="Lista todos os registros de Example.",
