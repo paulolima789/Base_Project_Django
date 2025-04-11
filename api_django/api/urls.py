5bb825bce1e134c52004c0977f03388236b576fb
paulolima789 <maicon271271@gmail.com>
2025-04-11 14:22:34 -0400

    Adiciona rotas para gerenciamento de grupos no arquivo de URLs

--- api_django/api/urls.py
+++ api_django/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .views.auth.google_login import GoogleLoginView
 from api.views.example import (
     ExampleListView,
     ExampleCreateView,
@@ -13,7 +14,13 @@ from api.views.user import (
     UserUpdateView,
     UserDeleteView,
 )
-
+from api.views.group import (
+    GroupListView,
+    GroupCreateView,
+    GroupDetailView,
+    GroupUpdateView,
+    GroupDeleteView,
+)
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -24,6 +31,7 @@ urlpatterns = [
     # Rota para obter o token (login)
     path('token/captcha/', TokenObtainPairWithCaptchaView.as_view(), name='token_obtain_pair'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain'),
+    path('token/google/', GoogleLoginView.as_view(), name='google-login'),
     # Rota para renovar o token
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
@@ -41,4 +49,10 @@ urlpatterns = [
     path("users/<int:pk>/update/", UserUpdateView.as_view(), name="user-update"),
     path("users/<int:pk>/delete/", UserDeleteView.as_view(), name="user-delete"),
 
+    # Groups
+    path("groups/", GroupListView.as_view(), name="group-list"),
+    path("groups/create/", GroupCreateView.as_view(), name="group-create"),
+    path("groups/<int:pk>/", GroupDetailView.as_view(), name="group-detail"),
+    path("groups/<int:pk>/update/", GroupUpdateView.as_view(), name="group-update"),
+    path("groups/<int:pk>/delete/", GroupDeleteView.as_view(), name="group-delete"),
 ]
